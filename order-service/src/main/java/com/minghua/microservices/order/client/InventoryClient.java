10a8f7625a981eb7f22e298a5201aa4056f574b0
yminghua <yminghua8@gmail.com>
2024-11-03 15:38:40 -0500

    refactor to use restclient

--- order-service/src/main/java/com/minghua/microservices/order/client/InventoryClient.java
+++ order-service/src/main/java/com/minghua/microservices/order/client/InventoryClient.java
@@ -1,12 +1,10 @@
 package com.minghua.microservices.order.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(value = "inventory-service", url = "${inventory.url}")
 public interface InventoryClient {
-    @RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
+
+    @GetExchange("/api/inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 }
