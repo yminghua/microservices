f03a64c63ab1c32941274f44d6939d79a72facbc
yminghua <yminghua8@gmail.com>
2024-11-02 20:19:20 -0400

    document REST API with OpenAPI swagger

---
+++ order-service/src/main/java/com/minghua/microservices/order/config/OpenAPIConfig.java
@@ -0,0 +1,23 @@
+package com.minghua.microservices.order.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+    @Bean
+    public OpenAPI orderServiceAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Order Service API")
+                        .description("REST APIs for Order Service")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("You can refer to the Order Service Documentation")
+                        .url("https://order-service-dummy-url.com/docs"));
+    }
+}
