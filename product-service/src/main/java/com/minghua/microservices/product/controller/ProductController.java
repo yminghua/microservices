d7ba52ec68632986a526d71608c65e0c11490eed
yminghua <yminghua8@gmail.com>
2024-11-03 19:37:49 -0500

    add short test code to test time-out for circuit breaker

--- product-service/src/main/java/com/minghua/microservices/product/controller/ProductController.java
+++ product-service/src/main/java/com/minghua/microservices/product/controller/ProductController.java
@@ -24,6 +24,12 @@ public class ProductController {
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<ProductResponse> getAllProducts() {
+        // Add delay to test time-out for circuit breaker
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
         return productService.getAllProducts();
     }
 }
